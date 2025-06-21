d5431cba660a345966fb54faa7f5bea07de0c40f
daftK <du_ikei@hotmail.com>
2025-06-21 17:17:27 -0300

    fix: add video field to VideoDataUUID record and reorder parameters

--- src/main/java/com/fiap/dto/VideoDataUUID.java
+++ src/main/java/com/fiap/dto/VideoDataUUID.java
@@ -3,8 +3,8 @@ package com.fiap.dto;
 import java.io.File;
 
 public record VideoDataUUID(
-        File video,
         String filename,
         String mimeType,
-        String uuid) {
+        String uuid,
+        String webhookUrl) {
 }
